96ed6b68868e0bbe268fdbaef643a64c87e6351e
gary60430 <gary60430@gmail.com>
2021-08-08 17:11:51 +0800

    test

---
+++ App_Start/WebApiConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace OnlineShop
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            //test2
+            config.Routes.MapHttpRoute(
+                name: "ApiByAction",
+                routeTemplate: "api/{controller}/{action}",
+                defaults: new { action = "Get" }
+            );
+        }
+    }
+}
